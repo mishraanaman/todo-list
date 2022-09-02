ab2e7fbf99abc1c4ab528b87e61e905563b11389
Naman Mishra <reachoutnaman@gmail.com>
2022-09-02 18:10:13 +0530

    Adding logger

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet')
-console.log("xxFFx");
-
+const morgan = require('morgan');
 const api = require('./routes/api');
-console.log("xxxxx");
+
 
 const app = express();
 
 app.use(helmet());
-
+console.log("Helmet instantiated....");
 
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
+app.use(morgan('combined'))
 app.use(express.json());
-console.log('xxxxx')
 
 app.use('/v1', api);
 
--- server.js
+++ server.js
@@ -2,15 +2,26 @@ console.log("----  Welcome to Tutor Bin Assignment ----");
 
 const fs = require("fs");
 const http = require("http");
+const mongoose = require("mongoose")
 require("dotenv").config();
-console.log("xxxxx");
-
+const MONGO_URL = process.env.MONGO_URL
 const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
-console.log(PORT)
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+mongoose.connection.once('open', ()=>{
+  console.log('Mongoose Connection is ready!')
+})
+mongoose.connection.on('error', (err) => {
+  console.error(`Mongoose Connection Error ${err}`);
 });
+
+async function startServer(){
+  await mongoose.connect(MONGO_URL)
+  server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer()
