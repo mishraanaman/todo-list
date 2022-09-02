20faa5ea8579ec6288280c04695b1255787c2f79
Naman Mishra <reachoutnaman@gmail.com>
2022-09-02 10:41:54 +0530

    JWT Authentication Added

--- utils/parser.js
+++
@@ -1,7 +0,0 @@
-function convertMapToJson(map) {
-  const json = JSON.stringify(Object.fromEntries(map));
-  const obj = JSON.parse(json);
-  return obj;
-}
-
-module.exports = { convertMapToJson };
